c5ffe76d8f3f06be61847bf52bf7c1b583e55a80
Nabeel <sheikhnabeel146@gmail.com>
2025-03-04 22:40:24 +0500

    update CORS middleware to use environment variables for origin and credentials

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express()
 app.use(express.static('public'))
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}))
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
